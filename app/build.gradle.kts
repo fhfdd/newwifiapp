2554cc5a5c8459ecc5696c5b039cd6f121e5c7ac
fhfdd <lvlianxizi@gmail.com>
2026-01-16 16:24:32 +0800

    换成vosksdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,7 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-
+    implementation("com.alphacephei:vosk-android:0.3.47")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
