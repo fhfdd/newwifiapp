bfa945e708d84e21700f9cf198d618727b8db90b
fhfdd <lvlianxizi@gmail.com>
2026-01-27 19:48:32 +0800

    加了2c 3c的指纹数据

--- app/src/main/java/com/example/indoornavblind/App.java
+++ app/src/main/java/com/example/indoornavblind/App.java
@@ -26,7 +26,7 @@ public class App extends Application {
 
     // 配置需要导入的指纹文件（确保文件名和路径正确）
 
-    private static final String[] FINGERPRINT_FILES = {"fingerprint_db.json","3a.json","26.json","test.json"};
+    private static final String[] FINGERPRINT_FILES = {"fingerprint_db.json","3c.json","2c.json","3a.json","26.json","test.json"};
 
 
     @Override
