12100a16a8172ac166333d26447c85dff436cb42
fhfdd <lvlianxizi@gmail.com>
2026-01-30 10:00:15 +0800

    指纹数据路径矫正

--- app/src/main/java/com/example/indoornavblind/App.java
+++ app/src/main/java/com/example/indoornavblind/App.java
@@ -26,7 +26,7 @@ public class App extends Application {
 
     // 配置需要导入的指纹文件（确保文件名和路径正确）
 
-    private static final String[] FINGERPRINT_FILES = {"3c.json","2c.json","3a.json","26.json","test.json"};
+    private static final String[] FINGERPRINT_FILES = {"3c.json","2c.json","3a.json","26.json"};
 
 
     @Override
